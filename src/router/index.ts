80cf598582b7bcec8bd97b85e43b86fef1e3a3db
Martin Alexanders Lopez Mamani <lopezalexanders@gmail.com>
2026-02-02 01:34:23 -0400

    proyecto actualizar vue final

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,7 @@ import PerfilView from '@/views/dashboard/PerfilView.vue'
 import TaskRegisterView from '@/views/dashboard/TaskRegisterView.vue'
 import TaskEditView from '@/views/dashboard/TaskeditView.vue'
 import TaskView from '@/views/dashboard/TaskView.vue'
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/dashboard' },
@@ -61,7 +61,8 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  //history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
